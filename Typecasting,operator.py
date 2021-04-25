42545aaf3ccc6aa16dc1c30fce300d2ad2a914e2
Balaji <balajikrishnan00@gmail.com>
2021-04-25 23:36:43 +0300

    update

--- Typecasting,operator.py
+++ Typecasting,operator.py
@@ -88,4 +88,47 @@ print(10>2)
 print(10<3)
 print(34<=12)
 print(54>=21)
+
+# equality operator
+# chaining  operator
+# == !=
+print(10>30>90)
+print(10<20>10)
+# member ship operators
+sen='God is Grace'
+#1p
+if 'God' in sen:
+    print('True')
+else:
+    print('False')
+#2p
+names=['arun','ravi','siva','guna','mahesh']
+if 'siva' in names:
+    print('yes')
+else:
+    print('no')
+#3p
+no=[10,43,56,74,34,35,6,754,323,]
+if 100 not in no:
+    print('yes')
+else:
+    print('no')
+
+# Ternary operator
+#1p
+a,b=21,20
+
+c=30 if a>b else 40
+print(c)
 '''
+#2p
+a=10
+b=5
+c=50
+
+min =a if a<b and a<c else b
+
+print(min)
+
+
+
