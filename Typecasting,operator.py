45af1a2fb48584044b9d36ee561dea6cbc6f9d5c
Balaji <balajikrishnan00@gmail.com>
2021-04-25 22:23:15 +0300

    file Uploded

---
+++ Typecasting,operator.py
@@ -0,0 +1,15 @@
+'''
+name='welcome'
+lenght=len(name) # len function using  no of charactor
+print(name ,'=',lenght)
+'''
+#Typecasting
+print(int(123.45))
+print(int(123.90))
+print(int('123455'))
+
+
+print(int(True))
+print(int(False))
+
+
