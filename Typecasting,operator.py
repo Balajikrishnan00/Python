96bff44ab3aada8b23497d9cc88bcb44358a39c9
Balaji <balajikrishnan00@gmail.com>
2021-04-25 22:33:26 +0300

    updated

--- Typecasting,operator.py
+++ Typecasting,operator.py
@@ -2,8 +2,9 @@
 name='welcome'
 lenght=len(name) # len function using  no of charactor
 print(name ,'=',lenght)
-'''
+
 #Typecasting
+# int()
 print(int(123.45))
 print(int(123.90))
 print(int('123455'))
@@ -11,5 +12,19 @@ print(int('123455'))
 
 print(int(True))
 print(int(False))
+
+# float()
+print(float(123))
+print(float(True))
+print(float(False))
+print(float('123'))
+
+#bool
+print(bool(0))
+print(bool(0.0))
+print(bool(5))
+print(bool(0.1))
+'''
+
 
 
