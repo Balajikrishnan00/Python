6e73719c6e48c71d2b434fe1a0d7cbb8df2b9f39
Balaji <balajikrishnan00@gmail.com>
2021-04-25 23:01:43 +0300

    updated

--- Typecasting,operator.py
+++ Typecasting,operator.py
@@ -24,12 +24,45 @@ print(bool(0))
 print(bool(0.0))
 print(bool(5))
 print(bool(0.1))
-'''
+
 #str()
 print(str(10))
 print(str(2.0))
 print(str(True))
 print(str(False))
 
+#type() type ex int ,float,str,bool
+# 14 data types available in python
+no=100
+name='shiva'
+tamil=True
+print(type(no))
+print(type(name))
+print(type(tamil))
+
+# memory address
+# id() memory address
+a=10
+b=20
+c=10
+print(id(a))
+print(id(b))
+print(id(c))
+
+# immutable ,mutable
+# cannot change,change of memory address
+state1='TamilNadu'
+state2='TamilNadu'
+print(id(state1))
+print(id(state2))
+#2586639203440
+#2586639203440
+
+a=(10,20)
+print(a)
+for i in a:
+    print(i)
+    print(id(i))
+'''
 
 
