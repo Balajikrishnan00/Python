2ef4f964124e4d2f5a130f16bd05c8b5a1917ec2
Balaji <balajikrishnan00@gmail.com>
2021-04-26 22:26:50 +0300

    updated

--- Operator,input,if else.py
+++ Operator,input,if else.py
@@ -11,6 +11,26 @@ print(total)
 #2p
 total=eval(input("Enter total"))
 print(total)
-'''
+
 import math
+
 print(math.pow(3,2))
+'''
+# end= ,sep=
+# formatted String
+# %i - integer
+# %d - integer
+# %s string
+# %f -float
+name='siva'
+age=24
+weight=50.6
+height=23
+print('This is formatted string')
+
+print('hi this is %s i am %i year old my height %d and weight %.2f.'%(name,age,height,weight))
+
+print('hi this is {} i am {} year old my height {} and weight {}'.format(name,age,height,weight))
+
+print(f'hi this is {name} i am {age} year old my height {height} and weight {weight}')
+
