c6dcc0ba0129338aeb6fcb3d430e97019f8f253d
Balaji <balajikrishnan00@gmail.com>
2021-04-28 17:52:19 +0300

    updated

--- Looping,Whileloop.py
+++ Looping,Whileloop.py
@@ -43,9 +43,25 @@ i=1
 while i<=100:
     print(i,end=' ')
     i+=2
-'''
+
 #8p
 i=3
 while i<=100:
     print(i,end=' ')
     i*=3
+
+#9p
+sNumber=1
+eNumber=int(input('End value:'))
+while sNumber<=eNumber:
+    if sNumber%3==0:
+        print(sNumber,end=' ')
+    sNumber+=1
+
+startN=1
+tableN=int(input('Table Number:'))
+endN=int(input('End Number:'))
+while startN<=endN:
+    print(f'{startN}*{tableN}={startN*tableN}')
+    startN+=1
+'''
