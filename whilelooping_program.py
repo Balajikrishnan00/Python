1adfffeea6eb6e7b8c7a0fb72d9769bae95ca7ea
Balaji <balajikrishnan00@gmail.com>
2021-04-30 18:34:23 +0300

    uploade

--- whilelooping_program.py
+++ whilelooping_program.py
@@ -48,4 +48,16 @@ if big%no1==0 and big%no2==0:
     print('LCM ',big)
 
 print('ji')
+
+#5p
+no1=int(input('Enter No1:'))
+no2=int(input('Enter No2:'))
+big=no1 if no1>no2 else no2
+
+while True:
+    if big%no1==0 and big%no2==0:
+        print('LCM',big)
+        break
+    big+=1
 '''
+
