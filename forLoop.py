1c32d0d6a4225afae75c37c2444e4a2d3c9b9fae
Balaji <balajikrishnan00@gmail.com>
2021-05-07 23:42:16 +0300

    file updated

--- forLoop.py
+++ forLoop.py
@@ -181,12 +181,21 @@ while row<5:
         col+=1
     print()
     row+=1
---------------------"""
+--------------------
 row=1
 while row<=5:
     col=1
     while col<=5:
         print('*',end='')
+        col+=1
+    print()
+    row+=1
+------------------"""
+row=1
+while row<=5:
+    col=1
+    while col<=row:
+        print(col,end='')
         col+=1
     print()
     row+=1
