4b7c572d4d46a83a7cc87b0d4ee23d229fa2b45a
Balaji <balajikrishnan00@gmail.com>
2021-05-07 15:00:28 +0300

    file updated

--- forLoop.py
+++ forLoop.py
@@ -1,5 +1,14 @@
+# For Loop Every Letter in Given string
+"""
 name='user1'
 
 for n in name:
     print(n)
+-----------------
+for x in range(10+1):
+    print(x,end=' ')
+-----------------"""
+
+
+
 
