080801281c30ba31526cd680694da23d25c317ec
Balaji <balajikrishnan00@gmail.com>
2021-05-08 18:32:10 +0300

    file updated

--- patternPrograms.py
+++ patternPrograms.py
@@ -43,7 +43,7 @@ while row>=1:
         col+=1
     print()
     row-=1
-----------------------------"""
+----------------------------
 row=5
 while row>=1:
     col=1
@@ -52,3 +52,16 @@ while row>=1:
         col+=1
     print()
     row-=1
+---------------------------
+row=5
+while row>=1:
+    col=5
+    while col>=row:
+        print(col,end=' ')
+        col-=1
+    print()
+    row-=1
+----------------------------"""
+
+
+
