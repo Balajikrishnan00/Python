fd2f0bbc96073bd8c1f77849b8416010fe8fd044
Balaji <balajikrishnan00@gmail.com>
2021-05-09 21:40:22 +0300

    file updated

--- patternPrograms.py
+++ patternPrograms.py
@@ -186,7 +186,7 @@ while row>=1:
         k+=1
     print()
     row-=1
-----------------------------------"""
+----------------------------------
 row=5
 while row>=1:
     col=5
@@ -198,5 +198,18 @@ while row>=1:
         print(row,end=' ')
         col2+=1
 
+    print()
+    row-=1
+------------------------------"""
+row=5
+while row>=1:
+    col=5
+    while col>row:
+        print(' ',end=' ')
+        col-=1
+    col2=1
+    while col2<=row:
+        print(row,end=' ')
+        col2+=1
     print()
     row-=1
