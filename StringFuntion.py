4492f64b8104bd2cdd5daab6257116650820a1d2
Balaji <balajikrishnan00@gmail.com>
2021-05-09 22:19:56 +0300

    file updated

--- StringFuntion.py
+++ StringFuntion.py
@@ -1,3 +1,16 @@
 # string is immutable
+"""
 name='Balajikrishnan'
 print(name.find('i'))
+----------------------
+sen='python is very easy'
+print(sen.count('y'))
+print(sen.find('y',5,len(sen)+1))
+-------------------------"""
+sen='python is very easy'
+c=0
+for w in range(0,len(sen)):
+    if sen[w]=='y':
+        c+=1
+print(c)
+
