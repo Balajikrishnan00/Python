9cfd138c4cbf01f5589ec4e1e89355d09a5f86b5
Balaji <balajikrishnan00@gmail.com>
2021-05-09 22:09:07 +0300

    file updated

--- forLoopPattern.py
+++ forLoopPattern.py
@@ -1,8 +1,14 @@
 """
 for n in range(1,10):
     print(n,end=' ')
-"""
+----------------------
 no=int(input('Enter a number'))
 for i in range(1,no+1):
     print('*'*i)
+-----------------------
+for row in range(1,10):
+    for col in range(1,row+1):
+        print(row,end=' ')
+    print()
+-------------------------"""
 
