33d98fca1765ad29c8b6bbd82b3b8e4ee06653cd
Balaji <balajikrishnan00@gmail.com>
2021-05-09 22:34:24 +0300

    file updated

--- StringFuntion.py
+++ StringFuntion.py
@@ -6,11 +6,12 @@ print(name.find('i'))
 sen='python is very easy'
 print(sen.count('y'))
 print(sen.find('y',5,len(sen)+1))
--------------------------"""
-sen='python is very easy'
+-------------------------
+sen='python is very very easy'
 c=0
 for w in range(0,len(sen)):
     if sen[w]=='y':
+        print(w)
         c+=1
 print(c)
-
+-------------------------------"""
