b1e1ea2df71107596cdc524e7e6d6fde4a45d6c1
Balaji <balajikrishnan00@gmail.com>
2021-05-11 00:01:48 +0300

    file updated

--- stringandListIndroduction.py
+++ stringandListIndroduction.py
@@ -1,4 +1,5 @@
 # merging two string
+"""
 s1=input('Enter s1 Name:')
 s2=input('Enter s2 Name:')
 s3=''
@@ -13,3 +14,15 @@ while i<max:
         s3+=s2[i]
     i+=1
 print(s3)
+------------------------"""
+alpha=''
+dight=''
+s='balajikrishnan123@gmail.com'
+for l in s:
+    if l.isalpha():
+        alpha+=l
+    if l.isdigit():
+        dight+=l
+print(alpha)
+print(dight)
+
