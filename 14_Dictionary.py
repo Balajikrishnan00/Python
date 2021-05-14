74f0924dae349595b88324018633653f7fff58a7
Balaji <balajikrishnan00@gmail.com>
2021-05-14 12:40:30 +0300

    file updated

--- 14_Dictionary.py
+++ 14_Dictionary.py
@@ -1,1 +1,16 @@
 # Dictionary
+# key ,value Pair
+# no duplicate key allowed
+# value duplicate allowed
+
+timeTable={'Tamil':'10:00am','English':'12:00am','Maths':'10:00am'}
+d={}
+print(type(d))
+for key in timeTable.keys():
+    print(key)
+    for value in timeTable.values():
+        print(value)
+d[1]='siva'
+d[2]='shankar'
+print(d)
+print(d[1])
