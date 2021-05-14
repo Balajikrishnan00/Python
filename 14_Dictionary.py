7e0a08313c33769c5012631627ff44923abdd9aa
Balaji <balajikrishnan00@gmail.com>
2021-05-14 13:37:27 +0300

    file updated

--- 14_Dictionary.py
+++ 14_Dictionary.py
@@ -2,7 +2,7 @@
 # key ,value Pair
 # no duplicate key allowed
 # value duplicate allowed
-
+"""
 timeTable={'Tamil':'10:00am','English':'12:00am','Maths':'10:00am'}
 d={}
 print(type(d))
@@ -14,3 +14,15 @@ d[1]='siva'
 d[2]='shankar'
 print(d)
 print(d[1])
+d[100]='balaji'
+print(d)
+# print(d[123]) Key Error
+print(d.keys())
+print(d.values())
+del d[100]
+print(d)
+d.clear()
+print(d)
+del d
+print(d)
+------------------------------------"""
