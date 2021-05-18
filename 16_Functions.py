e48f1c9c06112d908caef435b2eccbee2f925b1f
Balaji <balajikrishnan00@gmail.com>
2021-05-18 23:23:26 +0300

    file updated

--- 16_Functions.py
+++ 16_Functions.py
@@ -1,7 +1,21 @@
+"""
 def fun(name):
     print('hello Function',name)
 
-a1=input('Enter your Name:')
-fun(a1)
+#a1=input('Enter your Name:')
+#fun(a1)
+def sum(a,b):
+    return int(a)+int(b)
+v1=sum('1','2')
+#print(v1)
+def name(a='Gre'):
+    print(a)
+#name()
+def squere(n):
+    return n*n
+def cube(n):
+    return n*n*n
+print(squere(5))
+print(cube(5))
 
-
+------------------------"""
