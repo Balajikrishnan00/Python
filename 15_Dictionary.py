e02dc2d5de98ac36aa63ccf2727b4f85065763db
Balaji <balajikrishnan00@gmail.com>
2021-05-18 21:07:57 +0300

    file updated

--- 15_Dictionary.py
+++ 15_Dictionary.py
@@ -143,11 +143,12 @@ while True:
         #print(avg)
         min=avg+1
         avg=min+max//2
--------------------------"""
+-------------------------
 emp={'balaji':100,'kabilan':102,'Arun':103,'madhan':104}
 emp2={}
-empSalary={'balaji':10000,'kabilan':25000,'Arun':32000,'madhan':48000}
-
+empSalary={'balaji':10000,'kabilan':32000,'Arun':18000,'madhan':48000}
+nameList=[]
+salaryList=[]
 for x,y in emp.items():
     emp2[y]=x
 emp3={value:key for key,value in emp.items()}
@@ -157,4 +158,73 @@ for name,salary in empSalary.items():
 
     if salary>=25000:
         print(name)
+for n in emp2.values():
+    nameList.append(n)
+print(nameList)
 
+for salary in empSalary.values():
+    salaryList.append(salary)
+print(salaryList)
+
+for salary in sorted(empSalary.values()):
+    print(salary)
+------------------------
+# Bubble sort
+l=[10,5,8,3]
+
+#if l[0]>l[1]:
+#    l[1],l[0]=l[0],l[1]
+#if l[1]>l[2]:
+#    l[1], l[2] = l[2],l[1]
+#if l[2]>l[3]:
+#    l[3],l[2]=l[2],l[3]
+#print(l)
+i=0
+#while i<len(l)-1:
+
+    #if l[i]>l[i+1]:
+        #l[i+1],l[i]=l[i],l[i+1]
+    #i+=1
+#print(l)
+while i<len(l)-1:
+    j=i+1
+    while j<len(l):
+        if l[i]>l[j]:
+            l[j],l[i]=l[i],l[j]
+        j+=1
+    i+=1
+print(l)
+-------------------------
+l=[10,5,8,3]
+for i in range(len(l)-1):
+    for j in range(i+1,len(l)):
+        if l[i]>l[j]:
+            l[j],l[i]=l[i],l[j]
+        print(l)
+------------------------------
+
+interst=2
+amount={'Balaji':1000,'Ragu':2200,'Dinesh':1300,'John':2200,'sathish':1500,'jarorld':100,"jonoto":50}
+total=0
+t2=0
+Ins=0
+print(f'NAME\t\tAmount\tInsterst\tTotalAmount')
+print('--------------------------------------------')
+for name,amou in amount.items():
+    ins=amou*interst/100
+    #print(ins)
+    total=amou+ins
+
+    print(f'{name}\t:\t{float(amou)}\t{ins}:\t{total}')
+    #print('Total Amount')
+    t2+=total
+    Ins+=ins
+
+
+    #total+=amou*(interst)/100
+
+print('--------------------------------------------')
+print(Ins,'Total:',t2,'SAR')
+for val in sorted(amount.values()):
+    print(val)
+---------------------------------------"""
