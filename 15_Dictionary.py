24056b2ed51bd7d5a4bd51aed88941d96324e57e
Balaji <balajikrishnan00@gmail.com>
2021-05-18 22:03:50 +0300

    file updated

--- 15_Dictionary.py
+++ 15_Dictionary.py
@@ -228,3 +228,25 @@ print(Ins,'Total:',t2,'SAR')
 for val in sorted(amount.values()):
     print(val)
 ---------------------------------------"""
+l=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+key=32
+min=0
+max=len(l)-1
+#print(max)
+avg=(min+max)//2
+#print(avg)
+while min<=max:
+    if key==l[avg]:
+        print('Key is preseht',key)
+        break
+
+    elif key>l[avg]:
+        min=avg+1
+        avg=(min+max)//2
+    else:
+
+        max=avg-1
+        avg=(min+max)//2
+else:
+    print('KEY ERROR!...')
+    #print('KEY is Not Found',key)
