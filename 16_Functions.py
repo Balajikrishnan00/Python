43ad5d0fecdace2b74648c966bab8bcc64c4e739
Balaji <balajikrishnan00@gmail.com>
2021-05-19 21:52:27 +0300

    file updated

--- 16_Functions.py
+++ 16_Functions.py
@@ -18,4 +18,28 @@ def cube(n):
 print(squere(5))
 print(cube(5))
 
-------------------------"""
+------------------------
+def add(n,n1):
+    return n+n1
+print(add(10,20))
+print(type(add(1,2)))
+---------
+def odd_even(n):
+    if n%2==0:
+        print('Even')
+    else:
+        print('odd')
+
+#print(odd_even(5))
+odd_even(5)
+for i in range(10):
+    odd_even(i)
+-------------"""
+def calc(n1,n2):
+    add=n1+n2
+    sub=n1-n2
+    mul=n1*n2
+    div=n1/n2
+    return add,sub,mul,div
+print(calc(10,20))
+
