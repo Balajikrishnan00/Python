abcbc9b37b63123f269a86e0c1a56b5d617fca62
Balaji <balajikrishnan00@gmail.com>
2021-05-21 00:46:24 +0300

    file updated

--- 17_Recursion.py
+++ 17_Recursion.py
@@ -2,4 +2,23 @@
 l=[1,3,5,7,9]
 l1=[x*10+y for x in l for y in l]
 print(l1)
-"""
+
+------------------------------
+def username(u,p):
+    if u!='abcd':
+        print('Incorrect Username')
+        uName=input('UserName:')
+        pWord=input('Password:')
+        username(uName,pWord)
+    elif p!='abcd':
+        print('Incorrect Password')
+        uName = input('UserName:')
+        pWord = input('Password:')
+        username(uName, pWord)
+    else:
+        print('welcome ')
+
+uName=input('UserName:')
+pWord=input('Password:')
+username(uName,pWord)
+-----------------------------"""
