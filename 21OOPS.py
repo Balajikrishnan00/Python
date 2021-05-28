271ae1a0ef1b0a350886a8bc9f5c61904fd07deb
Balaji <balajikrishnan00@gmail.com>
2021-05-28 10:03:18 +0300

    file updated

--- 21OOPS.py
+++ 21OOPS.py
@@ -16,8 +16,37 @@ for x in dir(Human):
             print('dict')
     else:
         print('Other')
-------------------------------"""
-print('hello')
+------------------------------
+#print('hello')
 
-print(print('hai'),print('Welcome'))
+#print(print('hai'),print('Welcome'))
+# output
+#   hello
+#   hai
+#   Welcome
+#   None None
+------------------------------------"""
+class Human:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def DoWork(self):
+        print('Working')
+    def __str__(self):
+        return 'Welcome to Human Class'
+class Employee(Human):
+    def __init__(self,name,age,Empno,EmpSalary):
+        super(Employee,self).__init__(name,age)
+        self.empno=Empno
+        self.empsalary=EmpSalary
+    def __str__(self):
+        return 'Welcome to EMP Class'
 
+emp1=Employee('balaji',24,101,20000)
+print(emp1.empno)
+print(emp1.name)
+print(emp1)
+cus1=Human('siva',25)
+print(cus1)
+print(cus1.name)
+print(cus1.age)
