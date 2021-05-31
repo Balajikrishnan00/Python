0e5a898d91e25b0f37cfa785a99686a7b455417c
Balaji <balajikrishnan00@gmail.com>
2021-05-31 23:36:50 +0530

    updated file

--- 23_Exception_Handling.py
+++ 23_Exception_Handling.py
@@ -1,3 +1,4 @@
+"""
 try:
     no1 = int(input('Enter No1:'))
     no2 = int(input('Enter No2:'))
@@ -6,4 +7,24 @@ except ZeroDivisionError:
     print('Zero division Error')
 finally:
     print('Welcome to Balaji')
-    
+
+print(-0*-1)
+
+def sum(a):
+    print('balaji')
+
+sum(1)
+# Every Exception in Python is a Class
+# all Exception classes are child
+----------------------------------------"""
+try:
+    n1=int(input('Enter Number N1:'))
+    n2=int(input('Enter Number N2:'))
+
+    n3=n1/n2
+    print(n3)
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+except ValueError:
+    print('ValueError')
+print('hi')
