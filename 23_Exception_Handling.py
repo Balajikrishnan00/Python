dc3483d81a6ec1e701176511d829f8dc7859266a
Balaji <balajikrishnan00@gmail.com>
2021-05-31 20:31:02 +0530

    file updated

--- 23_Exception_Handling.py
+++ 23_Exception_Handling.py
@@ -1,3 +1,9 @@
-no1 = int(input('Enter No1:'))
-no2 = int(input('Enter No2:'))
-print(no1 / no2)
+try:
+    no1 = int(input('Enter No1:'))
+    no2 = int(input('Enter No2:'))
+    print(no1 / no2)
+except ZeroDivisionError:
+    print('Zero division Error')
+finally:
+    print('Welcome to Balaji')
+    
